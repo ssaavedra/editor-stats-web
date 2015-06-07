5a74179bcc4ed024e13e68ed5e2fcbc17d28f6db
Santiago Saavedra <santiagosaavedra@gmail.com>
2015-06-07 18:55:52 +0200

    Initial commit

---
+++ Handler/Sessions.hs
@@ -0,0 +1,9 @@
+module Handler.Sessions where
+
+import Import
+
+getSessionsR :: Handler Html
+getSessionsR = error "Not yet implemented: getSessionsR"
+
+postSessionsR :: Handler Html
+postSessionsR = error "Not yet implemented: postSessionsR"
---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "fileedit-logkeeper" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
