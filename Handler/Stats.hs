01af74e6736ddc3a057bdd08924c60e6ed214879
Santiago Saavedra <santiagosaavedra@gmail.com>
2015-07-15 19:58:22 +0200

    We now parse sexps

--- Handler/Stats.hs
+++ Handler/Stats.hs
@@ -2,8 +2,13 @@ module Handler.Stats where
 
 import Import
 import Handler.Documents (ListingAPI(..))
-import qualified Data.Text as T
+
 import Text.Shakespeare.Text
+import Language.Sexp.Parser (parseExn, Sexp(..))
+import qualified Data.ByteString.Lazy
+import qualified Data.Text.Lazy
+import qualified Data.Text.Lazy.Encoding as TL (decodeUtf8, encodeUtf8)
+
 
 checkOwningSession sessionId = do
   session <- runDB $ get404 sessionId
@@ -16,7 +21,7 @@ checkOwningSession sessionId = do
     permissionDenied [st|"You (#{userIdent user}) are not authorized to handle this session"|]
 
 
-emacsForm = runInputGet $ ireq textField "stats"
+postedStats = runInputPost $ ireq textField "stats"
 
 getSessionStatsR :: SessionId -> Handler TypedContent
 getSessionStatsR sessionId = do
@@ -38,13 +43,43 @@ getSessionStatsR sessionId = do
                <div>
                  <code>Authorization:
                  <kbd>Bearer #{sessionToken session}
+
+             <div>
+               <form action=@{SessionStatsR sessionId} method=POST>
             |]
 
 
-postSessionStatsR :: SessionId -> Handler Html
+postSessionStatsR :: SessionId -> Handler TypedContent
 postSessionStatsR sessionId = do
   session <- checkOwningSession sessionId
-  doc <- emacsForm
-  error $ show doc
+  stats <- postedStats
+
+  -- let sexp = parseExn . L.fromChunks . return . encodeUtf8 =<< return $ stats
+  --     stats' = mapMaybe (parseStat sessionId) sexp
+
+  let sexp = parseExn . Data.ByteString.Lazy.fromChunks . return . encodeUtf8 =<< return stats
+      stats' = mapMaybe (parseStat sessionId) sexp
+
+  -- TODO Persist on DB
+  r <- sequence $ map (runDB . insert) stats'
+  multiRepr r
+
+
+parseStat sid sexp = case sexp of
+  List [(Atom time),
+        List ((Atom stype):info)
+       ] ->
+    let time' = decodeUtf8 $ toStrict time
+        stype' = decodeUtf8 $ toStrict stype
+        info' = map sexpToText info
+    in
+      Just $ Stats sid time' stype' info'
+
+  _ -> Nothing
+
+sexpToText (Atom i) = Data.Text.Lazy.toStrict $ TL.decodeUtf8 i
+sexpToText (List l) = concat ["[", intercalate ", " $ map sexpToText l, "]"]
+
+
 
 
